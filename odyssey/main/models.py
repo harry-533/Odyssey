73ede12116c3b1be718ce387cf974b53b12e6b2c
James Mitchell <U1865744@unimail.hud.ac.uk>
2025-03-01 15:42:19 +0000

    First commit

--- odyssey/main/models.py
+++ odyssey/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class JourneyInformation(models.Model):
+    destination = models.CharField(max_length=50)
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+
 # Create your models here.
