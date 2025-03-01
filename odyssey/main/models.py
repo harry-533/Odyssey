69e5156830ba9209d87d26fea38a433d90606257
James Mitchell <U1865744@unimail.hud.ac.uk>
2025-03-01 17:08:10 +0000

    Created JourneyInformation model

--- odyssey/main/models.py
+++ odyssey/main/models.py
@@ -3,5 +3,10 @@ from django.db import models
 class JourneyInformation(models.Model):
     destination = models.CharField(max_length=50)
     budget = models.DecimalField(max_digits=10, decimal_places=2)
+    date_from = models.DateField()
+    date_to = models.DateField()
+
+    def __str__(self):
+        return f"{self.id} : {self.destination} with a budget of {self.budget}"
 
 # Create your models here.
