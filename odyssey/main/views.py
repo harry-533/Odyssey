9d2895db76075674c15f98c917b5baf442449d1e
James Mitchell <U1865744@unimail.hud.ac.uk>
2025-03-02 17:23:47 +0000

    Updated the home view

--- odyssey/main/views.py
+++ odyssey/main/views.py
@@ -1,7 +1,23 @@
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import JourneyInformation
 
 def home(request):
+    if request.method == 'POST':
+        destination = request.POST.get('destination')
+        budget = request.POST.get('budget')
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+
+        JourneyInformation.objects.create(
+            destination=destination,
+            budget=budget,
+            date_from=date_from,
+            date_to=date_to
+        )
+
+        return redirect('result.html')
+    
     return render(request, "home.html")
 
 def result(request):
