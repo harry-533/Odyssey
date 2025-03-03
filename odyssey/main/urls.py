38c5e8403d66cd78b415fb69e9448be67165bf9f
James Mitchell <U1865744@unimail.hud.ac.uk>
2025-03-03 12:14:06 +0000

    Implemented ModelForm and updated the view

--- odyssey/main/urls.py
+++ odyssey/main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('result/', views.result, name='result')
+    path('result/<int:pk>/', views.result, name='result')
 ]
--- odyssey/main/views.py
+++ odyssey/main/views.py
@@ -1,25 +1,23 @@
 from django.template import loader
-from django.shortcuts import render, redirect
-from models import JourneyInformation
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import JourneyInformationForm
+from .models import JourneyInformation
+
 
 def home(request):
     if request.method == 'POST':
-        destination = request.POST.get('destination')
-        budget = request.POST.get('budget')
-        date_from = request.POST.get('date_from')
-        date_to = request.POST.get('date_to')
+        form = JourneyInformationForm(request.POST)
 
-        JourneyInformation.objects.create(
-            destination=destination,
-            budget=budget,
-            date_from=date_from,
-            date_to=date_to
-            
-        )
-
-        return redirect('result')
+        if form.is_valid():
+            instance = form.save()
+            return redirect('result', pk=instance.pk)
+    else:
+        form = JourneyInformationForm()  
     
-    return render(request, "home.html")
+    return render(request, "home.html", {'form': form})
 
-def result(request):
-    return render(request, "result.html")
+def result(request, pk):
+
+    journey = get_object_or_404(JourneyInformation, pk=pk)
+
+    return render(request, "result.html", {'journey': journey})
