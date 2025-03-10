ee4f9da0914ec910db065ccc9fa57d7efd514e26
James Mitchell <U1865744@unimail.hud.ac.uk>
2025-03-10 10:40:19 +0000

    Overrided clean method in the form

--- odyssey/main/forms.py
+++ odyssey/main/forms.py
@@ -6,3 +6,7 @@ class JourneyInformationForm(forms.ModelForm):
     class Meta:
         model = JourneyInformation
         fields = '__all__'
+    
+    def clean(self):
+        for field, value in self.cleaned_data.items(): 
+            self.cleaned_data['field'] = value.lower() 
--- odyssey/main/views.py
+++ odyssey/main/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import JourneyInformationForm
 from .models import JourneyInformation
-# from .utils import cities
+from .utils import cities
 
 
 def home(request):
